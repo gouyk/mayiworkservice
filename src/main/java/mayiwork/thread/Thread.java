1108a428b8764485d5c4a6e9881439e7ef4f1a9d
gouyk <yingkuigou@creditease.cn>
2019-11-24 22:19:00 +0800

    增加个多线程的package

---
+++ src/main/java/mayiwork/thread/Thread.java
@@ -0,0 +1,12 @@
+package mayiwork.thread;
+
+/**
+ * @author yingkuigou
+ * @date 2019/11/24 22:17
+ */
+public class Thread {
+
+    public static void main(String[] args) {
+        System.out.println("begin");
+    }
+}
